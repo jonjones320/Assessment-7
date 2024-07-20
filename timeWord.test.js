ac7e688b7fdae5be0a5c6146933f4d0c70439c5d
JJ <jonjones320@gmail.com>
2024-07-20 03:07:40 -0700

    Separated the test elements into their own tests to better see what is failing.

--- timeWord.test.js
+++ timeWord.test.js
@@ -4,11 +4,19 @@ describe('#timeword', () => {
   test('it is a function', () => {
     expect(typeof timeWord).toBe('function');
   });
-  test('works', () => {
-      expect(timeWord("00:00")).toBe("twelve o'clock am");
-      expect(timeWord("01:05")).toBe("one oh-five am");
-      expect(timeWord("12:15")).toBe("twelve fifteen pm");
-      expect(timeWord("23:30")).toBe("eleven thirty pm");
-      expect(timeWord("13:00")).toBe("one o'clock pm");
+  test('works 00:00', () => {
+    expect(timeWord("00:00")).toBe("twelve o'clock am");
+  });
+  test('works 01:05', () => {
+    expect(timeWord("01:05")).toBe("one oh-five am");
+  });
+  test('works 12:15', () => {
+    expect(timeWord("12:15")).toBe("twelve fifteen pm");
+  });
+  test('works 23:30', () => {
+    expect(timeWord("23:30")).toBe("eleven thirty pm");
+  });
+  test('works 13:00', () => {
+    expect(timeWord("13:00")).toBe("one o'clock pm");
   });
 });
